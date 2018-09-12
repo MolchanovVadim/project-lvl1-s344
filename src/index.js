aa08678370c7072f7a15643f88f348893a7a5dab
Molchanov Vadim <molchanov_vadim@mail.ru>
2018-09-12 13:02:33 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import readlineSync from 'readline-sync';
 
-export default() => {
-  console.log('Welcome to the Brain Games!\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+export const hello = (game = '') => {
+  console.log('Welcome to the Brain Games!');
+  if (game === 'even') {
+    console.log('Answer "yes" if number even otherwise answer "no".');
+  }
+  const name = readlineSync.question('\nMay I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+  return name;
+};
+
+export const brainEven = () => {
+  const name = hello('even');
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * 101);
+    const itEven = (number % 2) === 0;
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((itEven && answer === 'yes') || (!itEven && answer === 'no')) {
+      console.log('Correct!');
+    } else {
+      const correctAnswer = itEven ? 'yes' : 'no';
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
 };
