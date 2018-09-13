2be4af3c7e4c33f5d7ee5972c3ba43f508d54e9f
Molchanov Vadim <molchanov_vadim@mail.ru>
2018-09-13 14:56:08 +0300

    step 6 change

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,20 +1,20 @@
 import game from '..';
 
+const getCorrectAnswer = (value, num1, num2, result) => {
+  if (value > num1 || value > num2) return result;
+  return getCorrectAnswer(value + 1, num1, num2,
+    (num1 % value === 0 && num2 % value === 0) ? value : result);
+};
+
+const getDataGame = () => {
+  const number1 = Math.floor(Math.random() * 100) + 1;
+  const number2 = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = getCorrectAnswer(2, number1, number2, 1);
+  const expression = `${number1} ${number2}`;
+  return [expression, String(correctAnswer)];
+};
+
 export default() => {
   const rules = 'Find the greatest common divisor of given numbers.';
-
-  const getDataGame = () => {
-    const number1 = Math.floor(Math.random() * 100) + 1;
-    const number2 = Math.floor(Math.random() * 100) + 1;
-
-    let correctAnswer = 1;
-    for (let i = 2; i <= number1 && i <= number2; i += 1) {
-      if (number1 % i === 0 && number2 % i === 0) correctAnswer = i;
-    }
-
-    const expression = `${number1} ${number2}`;
-    return [expression, String(correctAnswer)];
-  };
-
   game(rules, getDataGame);
 };
