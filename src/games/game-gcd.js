01b0c7abbfecb36b61bb4695c521c45ba0d6dd4b
Molchanov Vadim <molchanov_vadim@mail.ru>
2018-09-13 15:05:48 +0300

    change step 6

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,17 +1,17 @@
 import game from '..';
 
-const getCorrectAnswer = (value, num1, num2, result) => {
-  if (value > num1 || value > num2) return result;
-  return getCorrectAnswer(value + 1, num1, num2,
+const getCorrectAnswer = (num1, num2, value = 2, result = 1) => {
+  if (value > num1 || value > num2) return String(result);
+  return getCorrectAnswer(num1, num2, value + 1,
     (num1 % value === 0 && num2 % value === 0) ? value : result);
 };
 
 const getDataGame = () => {
   const number1 = Math.floor(Math.random() * 100) + 1;
   const number2 = Math.floor(Math.random() * 100) + 1;
-  const correctAnswer = getCorrectAnswer(2, number1, number2, 1);
+  const correctAnswer = getCorrectAnswer(number1, number2);
   const expression = `${number1} ${number2}`;
-  return [expression, String(correctAnswer)];
+  return [expression, correctAnswer];
 };
 
 export default() => {
